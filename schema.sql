dd5974575935cc5579300cae10c7277e29488fed
rod <rodrip0129@gmail.com>
2023-08-23 12:46:36 -0300

    Vet clinic database: query multiple tables

--- schema.sql
+++ schema.sql
@@ -12,5 +12,40 @@ CREATE TABLE animals (
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(100);
 
+--------------------------------------
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100),
+    age INTEGER
+);
+
+--------------------------------------
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+--------------------------------------
+
+ALTER TABLE animals
+ALTER COLUMN id SET DEFAULT nextval('animals_id_seq');
+
+--------------------------------------
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+--------------------------------------
+
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER REFERENCES species(id);
+
+--------------------------------------
+
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+
 
 
